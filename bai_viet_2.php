6e1ab31cf86e85c43576b72894b1b8010e5993d1
mrwall2510 <mrwall2510@gmail.com>
2024-10-07 09:19:37 +0700

    pj

--- bai_viet_2.php
+++ bai_viet_2.php
@@ -4,10 +4,97 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Travel - Bronx Luggage</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="/bootstrap4/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style1.css">
+    <link rel="stylesheet" href="css/main_styles.css">
 </head>
 <body>
-    <div class="container">
+<div class="main_nav_container">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12 text-right">
+							<div class="logo_container">
+								<a href="#">Bronx<span>Luggage</span></a>
+							</div>
+							<nav class="navbar">
+								<ul class="navbar_menu">
+									<li><a href="index.php">home</a></li>
+									<li><a href="#">shop</a></li>
+									<li><a href="#">FAQs</a></li>
+									<li><a href="#">pages</a></li>
+									<li><a href="#">blog</a></li>
+									<li><a href="contact.php">contact</a></li>
+								</ul>
+								<ul class="navbar_user">
+									<li><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a></li>
+									<li><a href="#"><i class="fa fa-user" aria-hidden="true"></i></a></li>
+									<li class="checkout">
+										<a href="#">
+											<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+											<span id="checkout_items" class="checkout_items">2</span>
+										</a>
+									</li>
+								</ul>
+								<div class="hamburger_container">
+									<i class="fa fa-bars" aria-hidden="true"></i>
+								</div>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</header>
+
+		<div class="fs_menu_overlay"></div>
+		<div class="hamburger_menu">
+			<div class="hamburger_close"><i class="fa fa-times" aria-hidden="true"></i></div>
+			<div class="hamburger_menu_content text-right">
+				<ul class="menu_top_nav">
+					<li class="menu_item has-children">
+						<a href="#">
+							usd
+							<i class="fa fa-angle-down"></i>
+						</a>
+						<ul class="menu_selection">
+							<li><a href="#">cad</a></li>
+							<li><a href="#">aud</a></li>
+							<li><a href="#">eur</a></li>
+							<li><a href="#">gbp</a></li>
+						</ul>
+					</li>
+					<li class="menu_item has-children">
+						<a href="#">
+							English
+							<i class="fa fa-angle-down"></i>
+						</a>
+						<ul class="menu_selection">
+							<li><a href="#">French</a></li>
+							<li><a href="#">Italian</a></li>
+							<li><a href="#">German</a></li>
+							<li><a href="#">Spanish</a></li>
+						</ul>
+					</li>
+					<li class="menu_item has-children">
+						<a href="#">
+							My Account
+							<i class="fa fa-angle-down"></i>
+						</a>
+						<ul class="menu_selection">
+							<li><a href="#"><i class="fa fa-sign-in" aria-hidden="true"></i>Sign In</a></li>
+							<li><a href="#"><i class="fa fa-user-plus" aria-hidden="true"></i>Register</a></li>
+						</ul>
+					</li>
+					<li class="menu_item"><a href="index.php">home</a></li>
+					<li class="menu_item"><a href="#">shop</a></li>
+					<li class="menu_item"><a href="#">FAQs</a></li>
+					<li class="menu_item"><a href="#">pages</a></li>
+					<li class="menu_item"><a href="#">blog</a></li>
+					<li class="menu_item"><a href="contact.php">contact</a></li>
+				</ul>
+			</div>
+		</div>
+    <div class="container1">
         <h1>Essential travel accessories for every trip</h1>
         <p>
         Sometimes, you are so excited about your upcoming trip that you forget the simple but very convenient accessories below. Regardless of whether it is a long trip or a short weekend trip. In addition to your main luggage, small and useful travel accessories can make your trip more convenient. Here are 10 accessories that Bronx Luggage recommends:
@@ -65,5 +152,46 @@
         <h4> These accessories not only help you maximize suitcase space but also ensure a safer, more comfortable, and organized trip. </h4>
         <h4><a href="index.php" class="btn">Back</a></h4>
     </div>
+    <!-- Footer -->
+
+		<footer class="footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6">
+						<div
+							class="footer_nav_container d-flex flex-sm-row flex-column align-items-center justify-content-lg-start justify-content-center text-center">
+							<ul class="footer_nav">
+								<li><a href="#">Blog</a></li>
+								<li><a href="#">FAQs</a></li>
+								<li><a href="contact.html">Contact us</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<div
+							class="footer_social d-flex flex-row align-items-center justify-content-lg-end justify-content-center">
+							<ul>
+								<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+								<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+								<li><a href="#"><i class="fa fa-skype" aria-hidden="true"></i></a></li>
+								<li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</footer>
+
+	</div>
+
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="styles/bootstrap4/popper.js"></script>
+	<script src="styles/bootstrap4/bootstrap.min.js"></script>
+	<script src="plugins/Isotope/isotope.pkgd.min.js"></script>
+	<script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+	<script src="plugins/easing/easing.js"></script>
+	<script src="js/custom.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -4,10 +4,97 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog - Bronx Luggage</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="/bootstrap4/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style1.css">
+    <link rel="stylesheet" href="css/main_styles.css">
 </head>
 <body>
-    <div class="container">
+<div class="main_nav_container">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12 text-right">
+							<div class="logo_container">
+								<a href="#">Bronx<span>Luggage</span></a>
+							</div>
+							<nav class="navbar">
+								<ul class="navbar_menu">
+									<li><a href="index.php">home</a></li>
+									<li><a href="#">shop</a></li>
+									<li><a href="#">FAQs</a></li>
+									<li><a href="#">pages</a></li>
+									<li><a href="#">blog</a></li>
+									<li><a href="contact.php">contact</a></li>
+								</ul>
+								<ul class="navbar_user">
+									<li><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a></li>
+									<li><a href="#"><i class="fa fa-user" aria-hidden="true"></i></a></li>
+									<li class="checkout">
+										<a href="#">
+											<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+											<span id="checkout_items" class="checkout_items">2</span>
+										</a>
+									</li>
+								</ul>
+								<div class="hamburger_container">
+									<i class="fa fa-bars" aria-hidden="true"></i>
+								</div>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</header>
+
+		<div class="fs_menu_overlay"></div>
+		<div class="hamburger_menu">
+			<div class="hamburger_close"><i class="fa fa-times" aria-hidden="true"></i></div>
+			<div class="hamburger_menu_content text-right">
+				<ul class="menu_top_nav">
+					<li class="menu_item has-children">
+						<a href="#">
+							usd
+							<i class="fa fa-angle-down"></i>
+						</a>
+						<ul class="menu_selection">
+							<li><a href="#">cad</a></li>
+							<li><a href="#">aud</a></li>
+							<li><a href="#">eur</a></li>
+							<li><a href="#">gbp</a></li>
+						</ul>
+					</li>
+					<li class="menu_item has-children">
+						<a href="#">
+							English
+							<i class="fa fa-angle-down"></i>
+						</a>
+						<ul class="menu_selection">
+							<li><a href="#">French</a></li>
+							<li><a href="#">Italian</a></li>
+							<li><a href="#">German</a></li>
+							<li><a href="#">Spanish</a></li>
+						</ul>
+					</li>
+					<li class="menu_item has-children">
+						<a href="#">
+							My Account
+							<i class="fa fa-angle-down"></i>
+						</a>
+						<ul class="menu_selection">
+							<li><a href="#"><i class="fa fa-sign-in" aria-hidden="true"></i>Sign In</a></li>
+							<li><a href="#"><i class="fa fa-user-plus" aria-hidden="true"></i>Register</a></li>
+						</ul>
+					</li>
+					<li class="menu_item"><a href="index.php">home</a></li>
+					<li class="menu_item"><a href="#">shop</a></li>
+					<li class="menu_item"><a href="#">FAQs</a></li>
+					<li class="menu_item"><a href="#">pages</a></li>
+					<li class="menu_item"><a href="#">blog</a></li>
+					<li class="menu_item"><a href="contact.php">contact</a></li>
+				</ul>
+			</div>
+		</div>
+    <div class="container1">
         <h1>Welcome to Bronx Luggage</h1>
         <p>
         Located at Festival Marketplace, we offer a wide range of luggage from affordable sets to the best brands on the market for all your travel needs. We also offer backpacks, travel accessories, briefcases and more.
@@ -61,6 +148,48 @@
             <img src="img/why.jpg" alt="Why choose Bronx Luggage for your luggage needs?" class="anh">
             <h4><a href="bai_viet_3.php" class="btn">More</a></h4>
     </div>
+</div>
+    <!-- Footer -->
+
+    <footer class="footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6">
+						<div
+							class="footer_nav_container d-flex flex-sm-row flex-column align-items-center justify-content-lg-start justify-content-center text-center">
+							<ul class="footer_nav">
+								<li><a href="#">Blog</a></li>
+								<li><a href="#">FAQs</a></li>
+								<li><a href="contact.html">Contact us</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<div
+							class="footer_social d-flex flex-row align-items-center justify-content-lg-end justify-content-center">
+							<ul>
+								<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+								<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+								<li><a href="#"><i class="fa fa-skype" aria-hidden="true"></i></a></li>
+								<li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</footer>
+
+	</div>
+
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="styles/bootstrap4/popper.js"></script>
+	<script src="styles/bootstrap4/bootstrap.min.js"></script>
+	<script src="plugins/Isotope/isotope.pkgd.min.js"></script>
+	<script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+	<script src="plugins/easing/easing.js"></script>
+	<script src="js/custom.js"></script>
 </body>
 </html>
 
